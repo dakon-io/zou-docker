a557f731793ca9080c2be96941a75864c6fc781a
Fanani M. Ihsan <fanani.mi@gmail.com>
2021-03-04 00:31:18 +0700

    fixes startup script

--- start_zou.sh
+++ start_zou.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo Running Zou...
+mkdir -p $PREVIEW_FOLDER
 sed -i "s/workers = .*/workers = $WORKERS/g" /etc/zou/gunicorn.conf
 sed -i "s/timeout = .*/timeout = $TIMEOUT/g" /etc/zou/gunicorn.conf
 sed -i "s/proxy_read_timeout .*/proxy_read_timeout $TIMEOUT\s;/g" /etc/nginx/http.d/default.conf
