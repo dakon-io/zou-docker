cd4225772b86fc299eae4c3a4a211438250d190c
Fanani M. Ihsan <fanani.mi@gmail.com>
2021-03-04 00:04:36 +0700

    fixes startup script

--- start_zou.sh
+++ start_zou.sh
@@ -4,19 +4,29 @@ echo Running Zou...
 sed -i "s/workers = .*/workers = $WORKERS/g" /etc/zou/gunicorn.conf
 sed -i "s/timeout = .*/timeout = $TIMEOUT/g" /etc/zou/gunicorn.conf
 sed -i "s/proxy_read_timeout .*/proxy_read_timeout $TIMEOUT\s;/g" /etc/nginx/http.d/default.conf
+GUNICORN_ACTIVE=false
+NGINX_ACTIVE=false
+GUNICORN_EVENT_ACTIVE=false
+RQ_ACTIVE=false
 for app in $(echo $APPS | sed "s/,/ /g")
 do
-    # call your procedure/other scripts here below
-    # echo "$app"
     if [ $app = "gunicorn" ]; then
-        sed -i 's/GUNICORN_ACTIVE/true/g' /etc/supervisord.conf
-        sed -i 's/NGINX_ACTIVE/true/g' /etc/supervisord.conf
+        GUNICORN_ACTIVE=true
+        NGINX_ACTIVE=true
     elif [ $app = "gunicorn-event" ]; then
-        sed -i 's/GUNICORN_EVENT_ACTIVE/true/g' /etc/supervisord.conf
+        GUNICORN_EVENT_ACTIVE=true
     elif [ $app = "rq" ]; then
-        sed -i 's/RQ_ACTIVE/true/g' /etc/supervisord.conf
+        RQ_ACTIVE=true
     else
         echo "app $app is not found!"
     fi
 done
+echo "Gunicorn active ? $GUNICORN_ACTIVE"
+echo "Nginx active    ? $NGINX_ACTIVE"
+echo "Event active    ? $GUNICORN_EVENT_ACTIVE"
+echo "RQ active       ? $RQ_ACTIVE"
+sed -i "s/GUNICORN_ACTIVE/$GUNICORN_ACTIVE/g" /etc/nginx/http.d/default.conf
+sed -i "s/NGINX_ACTIVE/$NGINX_ACTIVE/g" /etc/nginx/http.d/default.conf
+sed -i "s/GUNICORN_EVENT_ACTIVE/$GUNICORN_EVENT_ACTIVE/g" /etc/nginx/http.d/default.conf
+sed -i "s/RQ_ACTIVE/$RQ_ACTIVE/g" /etc/nginx/http.d/default.conf
 supervisord -c /etc/supervisord.conf
